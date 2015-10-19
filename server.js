43c2040ab6c420abed5b3112cfdf7fb083277a5a
Uriel <uriel.chavez@gmail.com>
2015-10-19 09:15:17 -0700

    Updated post.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require("mongoose");
-// var db = require("./models");
+ // var db = require("./models");
 var where = require("./utils/where.js");
 var path = require("path");
 mongoose.connect("mongodb://localhost/test");
@@ -20,8 +20,8 @@ console.log("variables loaded");
 // var Blog = mongoose.model('Blog', BlogSchema);
 
 var blogs =[
-	{blog: "When I die, I want to die peacefully, in my sleep, like my grandfather did.  Not like the passengers in his car.", author: "Jack Handy"},
-	{blog: "If you really want to impress people with your computer literacy, add the words dot-com to everything you say, dot-com.", author: "Jack Handy"},
+	{id: 0, blog: "When I die, I want to die peacefully, in my sleep, like my grandfather did.  Not like the passengers in his car.", author: "Jack Handy"},
+	{id: 1, blog: "If you really want to impress people with your computer literacy, add the words dot-com to everything you say, dot-com.", author: "Jack Handy"},
 
 ];
 
